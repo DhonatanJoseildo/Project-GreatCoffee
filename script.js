173fa1e94bf8fa17d83a943d96d66dc2d187637c
Dhonatan Joseildo <dhon.joseildo@gmail.com>
2022-05-27 22:50:43 -0300

    finishMenuMobile

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const menuWrapper = document.querySelector('.open-menu')
+let imgOpenMenu = document.querySelector('.open-menu img')
+menuWrapper.addEventListener('click', openMenu)
+console.log(imgOpenMenu)
 function openMenu() {
+
   menuMobile.classList.toggle('open')
+
+  if (menuMobile.classList.contains('open')) {
+    imgOpenMenu.src = './assets/closeMenu.svg'
+  }else{
+    imgOpenMenu.src = './assets/OpenMenu.svg'
+  }
+  
+  
 }
