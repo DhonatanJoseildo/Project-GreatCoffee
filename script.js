b2cfc5c55aed5e75d9d5700f01db11428a4f335f
Dhonatan Joseildo <dhon.joseildo@gmail.com>
2022-06-13 22:48:20 -0300

    upgrade

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ const checkContentMenu = document.querySelectorAll('main .menu .container')
 menuWrapper.addEventListener('click', openMenu)
 
 function openMenu() {
-
   menuMobile.classList.toggle('open')
 
   if (menuMobile.classList.contains('open')) {
     imgOpenMenu.src = './assets/closeMenu.svg'
-  }else{
-    imgOpenMenu.src = './assets/OpenMenu.svg'
+  } else {
+    imgOpenMenu.src = './assets/openMenu.svg'
   }
-  
 }
 
 function addCheckMenu() {
   checkContentMenu.forEach(element => {
-    element.innerHTML += "<span class='buttonChecked'>" + 
-    "<img src='./assets/checked.svg' alt='Imagem de Checked para compra de café'>"+
-    "</span>"
-  });
+    element.innerHTML +=
+      "<span class='buttonChecked'>" +
+      "<img src='./assets/checked.svg' alt='Imagem de Checked para compra de café'>" +
+      '</span>'
+  })
 }
 addCheckMenu()
