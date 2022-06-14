2e4bd5e364c9ba89917afbb1c9b6cfa6a10f88f7
Dhonatan Joseildo <dhon.joseildo@gmail.com>
2022-06-13 22:08:44 -0300

    updateFinish

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const menuWrapper = document.querySelector('.open-menu')
 let imgOpenMenu = document.querySelector('.open-menu img')
-
 const checkContentMenu = document.querySelectorAll('main .menu .container')
 
 menuWrapper.addEventListener('click', openMenu)
@@ -15,16 +14,13 @@ function openMenu() {
     imgOpenMenu.src = './assets/OpenMenu.svg'
   }
   
-  
 }
-console.log(checkContentMenu)
+
 function addCheckMenu() {
   checkContentMenu.forEach(element => {
     element.innerHTML += "<span class='buttonChecked'>" + 
     "<img src='./assets/checked.svg' alt='Imagem de Checked para compra de café'>"+
     "</span>"
-    return element
   });
-  
 }
 addCheckMenu()
